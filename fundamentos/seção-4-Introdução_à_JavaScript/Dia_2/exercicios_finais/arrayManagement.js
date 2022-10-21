8ef4d18aa8dfd29eac6188d5c1c1985373044804
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-10-21 11:05:39 -0300

    Itera sobre o array e descobre qual o maior valor e imprime ele

--- fundamentos/seção-4-Introdução_à_JavaScript/Dia_2/exercicios_finais/arrayManagement.js
+++ fundamentos/seção-4-Introdução_à_JavaScript/Dia_2/exercicios_finais/arrayManagement.js
@@ -1,10 +1,14 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 let media;
+let biggerValue;
 
 for(let number of numbers){
   console.log(number);
   sum += number;
+  if(number > biggerValue || numbers.indexOf(number) === 0){
+    biggerValue = number;
+  }
 }
 
 media = sum / numbers.length
@@ -16,3 +20,5 @@ if(media > 20){
 }else {
   console.log('Valor menor ou igual a 20');
 }
+
+console.log('Maior valor: ' + biggerValue);

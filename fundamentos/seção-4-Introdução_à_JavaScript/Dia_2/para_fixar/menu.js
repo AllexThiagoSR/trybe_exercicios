3148189d9229629af26d4d72395b17a7a4944c2d
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-10-21 08:48:41 -0300

    Finaliza a segunda e a terceira parte do primeiro exercício para fixar

--- fundamentos/seção-4-Introdução_à_JavaScript/Dia_2/para_fixar/menu.js
+++ fundamentos/seção-4-Introdução_à_JavaScript/Dia_2/para_fixar/menu.js
@@ -1,4 +1,9 @@
 const menu = ['Home', 'Serviços', 'Portifólio', 'Links']
-const menuServices = menu[menu.indexOf('Links')];
+const menuServices = menu[menu.indexOf('Serviços')];
+const indexOfPortifolio = menu.indexOf('Portifólio');
+
+menu.push('Contato');
 
 console.log(menuServices);
+console.log(indexOfPortifolio);
+console.log(menu);

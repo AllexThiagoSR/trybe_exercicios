e29dfc15a9296cb57c2466859ab3dd76c41770a3
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-10-24 10:58:03 -0300

    Remaneja o código para ficar um pouco menor

--- fundamentos/seção-4-Introdução_à_JavaScript/Dia_3/exercicios_finais/numPrimo.js
+++ fundamentos/seção-4-Introdução_à_JavaScript/Dia_3/exercicios_finais/numPrimo.js
@@ -1,7 +1,7 @@
-let counter = 0;
 let largestPrime = 1;
 
 for(let i = 2; i <=  50; i += 1){
+  let counter = 0;
   for(let j = 1; j <= i; j += 1){
     if(i % j === 0){
       counter += 1;
@@ -10,7 +10,6 @@ for(let i = 2; i <=  50; i += 1){
   if(counter <= 2 && i > largestPrime){
     largestPrime = i;
   }
-  counter = 0;
 }
 
 console.log('Maior número primo entre 2 e 50: ', largestPrime);

62d471eae822c074db22faf50a4697311e0f91bd
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-10-25 18:31:21 -0300

    Função que verifica o fim de uma string

---
+++ fundamentos/seção-4-Introdução_à_JavaScript/Dia_4/exercicios_functions/ending.js
@@ -0,0 +1,11 @@
+function endWithThis(word, ending){
+  if(word.length > ending.length){  
+    return word.endsWith(ending);
+  }else{
+    return 'Ending deve ser menor que word';
+  }
+}
+
+
+console.log(endWithThis('trybe', 'be'));
+console.log(endWithThis('joaofernando', 'fernan'));

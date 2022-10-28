e4b870e9708d0a7888e2b688b20a6dd6dbe84adb
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-10-28 13:30:08 -0300

    Adiciona uma cor de fundo no footer

--- fundamentos/seção-5-JavaScript-DOM_Eventos_e_Web_Storage/exercicio1/script.js
+++ fundamentos/seção-5-JavaScript-DOM_Eventos_e_Web_Storage/exercicio1/script.js
@@ -1,16 +1,30 @@
+//Header
 const headerCont = document.querySelector('#header-container');
+
 headerCont.style.backgroundColor = '#00B069';
 
+//Emergency tasks
 const emergencyTasks = document.querySelector('.emergency-tasks');
+
 emergencyTasks.style.backgroundColor = '#FF9F84';
+
 const divEmergency = emergencyTasks.querySelectorAll('div h3');
+
 for(let h3 of divEmergency){
   h3.style.backgroundColor = '#A500F3';
 }
 
+//No Emergency tasks
 const noEmergencyTasks = document.querySelector('.no-emergency-tasks');
+
 noEmergencyTasks.style.backgroundColor = '#F9DB5E';
+
 const divNoEmergency = noEmergencyTasks.querySelectorAll('div h3');
+
 for(let h3 of divNoEmergency){
   h3.style.backgroundColor = 'black';
 }
+
+//Footer
+const footerCont = document.querySelector('#footer-container');
+footerCont.style.backgroundColor = '#003533';

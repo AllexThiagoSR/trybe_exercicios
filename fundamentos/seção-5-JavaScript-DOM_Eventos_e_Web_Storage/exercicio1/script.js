a33a8fdf50a045ae138f21499a9bc2877a8a590c
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-10-28 13:20:41 -0300

    Estiliza as emergency e no-emergency tasks com JavaScript

--- fundamentos/seção-5-JavaScript-DOM_Eventos_e_Web_Storage/exercicio1/script.js
+++ fundamentos/seção-5-JavaScript-DOM_Eventos_e_Web_Storage/exercicio1/script.js
@@ -1,2 +1,16 @@
 const headerCont = document.querySelector('#header-container');
 headerCont.style.backgroundColor = '#00B069';
+
+const emergencyTasks = document.querySelector('.emergency-tasks');
+emergencyTasks.style.backgroundColor = '#FF9F84';
+const divEmergency = emergencyTasks.querySelectorAll('div h3');
+for(let h3 of divEmergency){
+  h3.style.backgroundColor = '#A500F3';
+}
+
+const noEmergencyTasks = document.querySelector('.no-emergency-tasks');
+noEmergencyTasks.style.backgroundColor = '#F9DB5E';
+const divNoEmergency = noEmergencyTasks.querySelectorAll('div h3');
+for(let h3 of divNoEmergency){
+  h3.style.backgroundColor = 'black';
+}

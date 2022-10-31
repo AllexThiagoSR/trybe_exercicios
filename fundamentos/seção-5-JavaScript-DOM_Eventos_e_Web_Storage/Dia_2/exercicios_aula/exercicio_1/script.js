9457b2f8f74f8bf139d873c1cdece56cbe0e9c42
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-10-31 10:03:24 -0300

    Obtem o elementoOndeVoceEsta e seu pai

---
+++ fundamentos/seção-5-JavaScript-DOM_Eventos_e_Web_Storage/Dia_2/exercicios_aula/exercicio_1/script.js
@@ -0,0 +1,5 @@
+const elementWhereRU = document.getElementById('elementoOndeVoceEsta');
+const dadElement = elementWhereRU.parentElement;
+dadElement.style.color = 'red';
+
+console.log(elementWhereRU);

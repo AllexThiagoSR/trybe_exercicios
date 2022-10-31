3a2b53afe56c91622b1c8058553d7fd3026bf221
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-10-31 15:48:58 -0300

    Cria e adiciona uma section dentro de maimn

--- fundamentos/seção-5-JavaScript-DOM_Eventos_e_Web_Storage/Dia_2/exercicios_finais/script.js
+++ fundamentos/seção-5-JavaScript-DOM_Eventos_e_Web_Storage/Dia_2/exercicios_finais/script.js
@@ -1,11 +1,12 @@
 //Adicionar uma tag h1 como sendo filho do body
 const creatAElement = (tagName, parent, _text='', _id='', _classs='') => {
   const element = document.createElement(tagName);
-  element.innerText = _text;
-  element.id = _id;
-  element.className = _classs;
+  _text !== '' ? element.innerText = _text : '';
+  _id !== '' ? element.id = _id : '';
+  _classs !== '' ? element.className = _classs: '';
   parent.appendChild(element);
 };
 
-creatAElement('h1', document.body, 'Exercício - JavaScript DOM', id='title-in-body');
+creatAElement('h1', document.body, 'Exercício - JavaScript DOM', 'title-in-body');
 creatAElement('main', document.body, '', '', 'main-content');
+creatAElement('section', document.getElementsByClassName('main-content')[0], '', '', 'center-content')

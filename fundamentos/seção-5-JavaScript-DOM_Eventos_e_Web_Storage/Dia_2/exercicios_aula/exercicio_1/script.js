f9748e46382de8e390ae9476139537f6f65caf92
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-10-31 10:20:47 -0300

    Obtém todos os elementos desejados

--- fundamentos/seção-5-JavaScript-DOM_Eventos_e_Web_Storage/Dia_2/exercicios_aula/exercicio_1/script.js
+++ fundamentos/seção-5-JavaScript-DOM_Eventos_e_Web_Storage/Dia_2/exercicios_aula/exercicio_1/script.js
@@ -3,3 +3,21 @@ const dadElement = elementWhereRU.parentElement;
 dadElement.style.color = 'red';
 
 console.log(elementWhereRU);
+
+
+const firstChildOfChild = elementWhereRU.firstElementChild;
+firstChildOfChild.innerText = 'Testando 123';
+
+
+
+const atention = elementWhereRU.nextSibling;
+console.log(atention);
+
+
+const thirdChild = elementWhereRU.nextElementSibling;
+console.log(thirdChild);
+
+
+const thirdChildWithDad = document.getElementById('pai').firstElementChild.nextElementSibling.nextElementSibling;
+
+console.log(thirdChildWithDad);

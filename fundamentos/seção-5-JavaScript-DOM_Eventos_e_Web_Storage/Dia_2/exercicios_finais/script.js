7db10848b3219e051a0c9e5c38c4809149356212
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-10-31 16:00:04 -0300

    Adiciona imagem à section left-content

--- fundamentos/seção-5-JavaScript-DOM_Eventos_e_Web_Storage/Dia_2/exercicios_finais/script.js
+++ fundamentos/seção-5-JavaScript-DOM_Eventos_e_Web_Storage/Dia_2/exercicios_finais/script.js
@@ -7,9 +7,18 @@ const creatAElement = (tagName, parent, _text='', _id='', _classs='') => {
   parent.appendChild(element);
 };
 
+const creatAElementWithImage = (src, parent, _id='', _classs='') => {
+  const element = document.createElement('img');
+  element.src = src;
+  _id !== '' ? element.id = _id : '';
+  _classs !== '' ? element.className = _classs: '';
+  parent.appendChild(element);
+};
+
 creatAElement('h1', document.body, 'Exercício - JavaScript DOM', 'title-in-body');
 creatAElement('main', document.body, '', '', 'main-content');
 creatAElement('section', document.getElementsByClassName('main-content')[0], '', '', 'center-content');
 creatAElement('p', document.getElementsByClassName('center-content')[0], 'Alô, testando 1, 2, 3');
 creatAElement('section', document.getElementsByClassName('main-content')[0], '', '', 'left-content');
 creatAElement('section', document.getElementsByClassName('main-content')[0], '', '', 'right-content');
+creatAElementWithImage('https://picsum.photos/200', document.getElementsByClassName('left-content')[0], '', 'small-image');

dfeef9146e430965f05220e10b390ec26c78eb5b
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-02 16:58:25 -0300

    Explicações

--- fundamentos/seção-5-JavaScript-DOM_Eventos_e_Web_Storage/Dia_1/exercicio_final/script.js
+++ fundamentos/seção-5-JavaScript-DOM_Eventos_e_Web_Storage/Dia_1/exercicio_final/script.js
@@ -1,3 +1,66 @@
+// /*
+// Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+// - document.getElementById()
+// - document.getElementsByClassName()
+// - document.getElementsByTagName()
+// 1. Crie e execute uma função que mude o texto na tag `<p>-----</p>`, para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+// 2. Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+// 3. Crie e execute uma função que mude a cor do quadrado vermelho para branco.
+// 4. Crie e execute uma função que corrija o texto da tag <h1>.
+// 5. Crie e execute uma função que modifique o texto da primeira tag <p> para maiúsculo.
+// 6. Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
+// */
+
+// //Exercício 1
+// const changeParagraph = (id = '', text = '') => {
+//   const paragraph = document.getElementById(id);
+//   paragraph.innerText = text;
+// };
+
+// changeParagraph('text-mod2', 'Me vejo trabalhando, com uma vida tranquila e feliz');
+
+// //Exercício 2
+// const changeClassBackgroundColor = (classs, color) => {
+//   const classElements = document.getElementsByClassName(classs);
+//   for (let elem of classElements) {
+//     elem.style.backgroundColor = color;
+//   }
+// };
+
+// changeClassBackgroundColor('main-content', 'rgb(76,164,109)')
+
+// //Exercício 3
+// changeClassBackgroundColor('center-content', 'white');
+
+// //Exercício 4
+// const replaceText = (id, text1, text2) => {
+//   const element = document.getElementById(id);
+//   let tagText = element.innerText;
+//   tagText = tagText.replace(text1, text2);
+//   element.innerText = tagText;
+// };
+
+// replaceText('title-head', 'Escripito', 'Script');
+
+// //Exercício 5
+// const transformIdText = (id, transformation) => {
+
+//   document.getElementById(id).style.textTransform = transformation;
+// };
+
+// transformIdText('text-mod1', 'uppercase');
+
+// //Exercício 6
+// const showTagsContent = (tag) => {
+//   const tagsArray = document.getElementsByTagName(tag);
+//   for (let objectTag of tagsArray) {
+//     console.log(objectTag.innerText);
+//   }
+// };
+
+// showTagsContent('p');
+
+
 /*
 Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
 - document.getElementById()
@@ -11,51 +74,63 @@ Aqui você vai modificar os elementos já existentes utilizando apenas as funç�
 6. Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
 */
 
-//Exercício 1
-const changeParagraph = (id = '', text = '') => {
-  const paragraph = document.getElementById(id);
-  paragraph.innerText = text;
-};
+function modificarP (texto, novoTexto){
+  const paragrafo = document.getElementsByTagName('p');
+  const textoModificar = texto;
 
-changeParagraph('text-mod2', 'Me vejo trabalhando, com uma vida tranquila e feliz');
-
-//Exercício 2
-const changeClassBackgroundColor = (classs, color) => {
-  const classElements = document.getElementsByClassName(classs);
-  for (let elem of classElements) {
-    elem.style.backgroundColor = color;
+  // console.log(paragrafo);
+  for (let index in paragrafo){
+    if (paragrafo[index].innerText === textoModificar) {
+      paragrafo[index].innerHTML = novoTexto;
+    }
   }
+}
+
+modificarP('oi', '123456');
+
+
+// 2. Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+function alteraCorQuadrado(elemento, cor){
+  const quadrado = document.getElementsByClassName(elemento)[0];
+  quadrado.style['background-color'] = cor;
+}
+
+alteraCorQuadrado('main-content', 'rgb(76,164,109)');
+
+// 3. Crie e execute uma função que mude a cor do quadrado vermelho para branco.
+
+alteraCorQuadrado('center-content', 'white');
+
+// 4. Crie e execute uma função que corrija o texto da tag <h1>.
+
+const modificaH1 = () => {
+  const elementoH1 = document.getElementsByClassName('title')[0];
+  elementoH1.innerText = 'Exercício - JavaScript';
 };
 
-changeClassBackgroundColor('main-content', 'rgb(76,164,109)')
+modificaH1();
 
-//Exercício 3
-changeClassBackgroundColor('center-content', 'white');
+// 5. Crie e execute uma função que modifique o texto da primeira tag <p> para maiúsculo.
 
-//Exercício 4
-const replaceText = (id, text1, text2) => {
-  const element = document.getElementById(id);
-  let tagText = element.innerText;
-  tagText = tagText.replace(text1, text2);
-  element.innerText = tagText;
-};
+function modificaTexto() {
+  let paragrafo = document.getElementsByTagName('p')[0];
 
-replaceText('title-head', 'Escripito', 'Script');
+  paragrafo.innerText = paragrafo.innerText.toUpperCase();
+  // paragrafo.style.textTransform = 'lowerCase';
+}
 
-//Exercício 5
-const transformIdText = (id, transformation) => {
+modificaTexto();
 
-  document.getElementById(id).style.textTransform = transformation;
-};
+// 6. Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
 
-transformIdText('text-mod1', 'uppercase');
-
-//Exercício 6
-const showTagsContent = (tag) => {
-  const tagsArray = document.getElementsByTagName(tag);
-  for (let objectTag of tagsArray) {
-    console.log(objectTag.innerText);
+function mostraParagrafo(){
+  const paragrafos = document.getElementsByTagName('p');
+  console.log(paragrafos);
+  for (let index = 0; index < paragrafos.length; index += 1){
+    console.log(paragrafos[index].innerHTML);
   }
-};
+}
 
-showTagsContent('p');
+// mostraParagrafo();
+
+console.log(document.querySelectorAll('.main-content .center-content p'));

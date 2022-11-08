a2497d67bee5e74b74a61510e91f9b3554c65509
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-08 16:40:02 -0300

    Exibe mensagens se os campos com tamanhos minímos forem inválidos

--- fundamentos/seção-6-HTML_e_CSS-Forms_Flexbox_e_Responsivo/Dia_1/script.js
+++ fundamentos/seção-6-HTML_e_CSS-Forms_Flexbox_e_Responsivo/Dia_1/script.js
@@ -2,5 +2,10 @@ const submitButton = document.getElementById('submit-button');
 submitButton.addEventListener('click', (event) => {
   event.preventDefault();
   const nameInput = document.getElementById('input-name');
-  const emailInput = document.getElementById('email-input');
+  const emailInput = document.getElementById('input-email');
+  if (nameInput.value.length < 10 || emailInput.value.length < 10) {
+    alert('Dados inválidos!');
+  } else {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+  }
 });

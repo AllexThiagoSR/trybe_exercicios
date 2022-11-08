369694203d4014b665658a9a4fda896f822366b2
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-08 16:11:02 -0300

    Previne o comportamento padrão de submit e adiciona botão que limpa os campos

---
+++ fundamentos/seção-6-HTML_e_CSS-Forms_Flexbox_e_Responsivo/Dia_1/script.js
@@ -0,0 +1,6 @@
+const submitButton = document.getElementById('submit-button');
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const nameInput = document.getElementById('input-name');
+  const emailInput = document.getElementById('email-input');
+});

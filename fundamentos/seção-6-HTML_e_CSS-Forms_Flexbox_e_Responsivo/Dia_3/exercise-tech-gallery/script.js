7928ab9b59f79e6c2bbf604ce776f484963690ca
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-10 16:08:50 -0300

    Modifica o arredondamento dos cards

--- fundamentos/seção-6-HTML_e_CSS-Forms_Flexbox_e_Responsivo/Dia_3/exercise-tech-gallery/script.js
+++ fundamentos/seção-6-HTML_e_CSS-Forms_Flexbox_e_Responsivo/Dia_3/exercise-tech-gallery/script.js
@@ -1,5 +1,5 @@
 const divs = document.getElementsByTagName('div');
 
 for (let index in divs) {
-  divs[index].className = 'shadow p-3 rounded';
+  divs[index].className = 'shadow p-3 rounded-4';
 }

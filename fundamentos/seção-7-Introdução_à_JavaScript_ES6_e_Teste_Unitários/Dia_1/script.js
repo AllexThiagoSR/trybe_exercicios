9875496c489c01fdfb816598c3c96f407ea1de1b
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-18 15:38:50 -0300

    Lança um erro se os inputs forem vazios

--- fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_1/script.js
+++ fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_1/script.js
@@ -1,10 +1,22 @@
+const checkValues = (value1, value2) => {
+  if (value1 === '' || value2 === '') {
+    throw new Error('Algum dos valores não foi informado');
+  }
+}
+
 function sum() {
   const value1 = document.getElementById('value1').value;
   const value2 = document.getElementById('value2').value;
-  const result = Number(value1) + Number(value2);
-  document.getElementById('result').innerHTML = `Resultado: ${result}`;
-  document.getElementById('value1').value = '';
+  try {
+    checkValues(value1, value2);
+    const result = Number(value1) + Number(value2);
+    document.getElementById('result').innerHTML = `Resultado: ${result}`;
+  } catch (e) {
+    document.getElementById('result').innerHTML = e.message;
+  } finally {
+    document.getElementById('value1').value = '';
   document.getElementById('value2').value = '';
+  } 
 }
 window.onload = () => {
   const button = document.getElementById('button');

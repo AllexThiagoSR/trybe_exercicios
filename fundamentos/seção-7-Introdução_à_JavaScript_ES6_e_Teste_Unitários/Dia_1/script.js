59c435cdc64d6b1e9604bf0c2d2d200da04e1f7b
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-18 15:46:07 -0300

    Lança se não forem digitados valores numéricos nos inputs

--- fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_1/script.js
+++ fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_1/script.js
@@ -1,15 +1,22 @@
 const checkValues = (value1, value2) => {
   if (value1 === '' || value2 === '') {
     throw new Error('Algum dos valores não foi informado');
+  } else if (isNaN(Number(value1)) || isNaN(Number(value2))) {
+    throw new Error('Os valores informado precisam ser numéricos');
   }
 }
 
-function sum() {
+const sum = (value1, value2) => {
+  const result = Number(value1) + Number(value2);
+  return result;
+};
+
+function catchSumExceptions() {
   const value1 = document.getElementById('value1').value;
   const value2 = document.getElementById('value2').value;
   try {
     checkValues(value1, value2);
-    const result = Number(value1) + Number(value2);
+    const result = sum(value1, value2);
     document.getElementById('result').innerHTML = `Resultado: ${result}`;
   } catch (e) {
     document.getElementById('result').innerHTML = e.message;
@@ -20,5 +27,5 @@ function sum() {
 }
 window.onload = () => {
   const button = document.getElementById('button');
-  button.addEventListener('click', sum);
+  button.addEventListener('click', catchSumExceptions);
 }

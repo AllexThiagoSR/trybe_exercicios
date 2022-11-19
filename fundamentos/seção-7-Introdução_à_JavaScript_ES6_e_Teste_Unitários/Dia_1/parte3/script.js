753d29d86f163c1ec6910d892069776353b1dd80
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-19 16:12:19 -0300

    Cria um objeto allLessons]

--- fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_1/parte3/script.js
+++ fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_1/parte3/script.js
@@ -49,3 +49,11 @@ addProperty(lesson2, 'turno', 'noite');
 // console.log(objectLength(lesson1));
 
 // eachValue(lesson2);
+
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3}); // Desse modo o objeto que esta sendo criado assim {objeto1, objeto2, objeto3}, é criado automaticamente uma chave com o mesmo nome da constante ou variável que guarda o objeto, isso funciona para todos os tipos de primitivos.
+
+// let variavel = 'isso é uma variável'; // Declarei uma variável
+// const bejeto = { a: 1, b: 2, c:3, variavel}; // Se eu não pôr do jeito convêncional chave: valor, e o o que eu desejo inserir for uma variável ou constante declarada antes isso é considerado uma entrada inteira de chave: valor, a chave será o nome da variável ou constante e o valor será o valor que atribui a ela
+// console.log(bejeto);
+
+console.log(allLessons);

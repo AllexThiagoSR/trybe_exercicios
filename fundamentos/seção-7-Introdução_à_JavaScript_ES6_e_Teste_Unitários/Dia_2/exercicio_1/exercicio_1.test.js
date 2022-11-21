c964716b6e7b1525785e72aab8b5d14c210c2c52
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-21 16:10:21 -0300

    Cria segundo teste do exercicio 1

--- fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_2/exercicio_1/exercicio_1.test.js
+++ fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_2/exercicio_1/exercicio_1.test.js
@@ -2,9 +2,16 @@ const myRemove = require('./exercicio_1.js');
 
 console.log(myRemove);
 
-it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
   const array = [1, 2, 3, 4];
   const numberToRemove = 3;
 
   expect(myRemove(array, numberToRemove)).not.toContain(numberToRemove);
+});
+
+it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+  const array = [1, 2, 3, 4];
+  const numberToRemove = 3;
+
+  expect(myRemove(array, numberToRemove)).not.toEqual(array)
 });

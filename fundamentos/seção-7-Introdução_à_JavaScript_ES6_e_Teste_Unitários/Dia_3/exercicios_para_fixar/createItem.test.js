2a0451f06037affe1c225ba170edbb0d98eb7a38
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-22 20:12:47 -0300

    Cria o último teste do exercício

--- fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_3/exercicios_para_fixar/createItem.test.js
+++ fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_3/exercicios_para_fixar/createItem.test.js
@@ -9,9 +9,13 @@ describe('a função createItem', () => {
   it('Teste: Se lança um erro quando não recebe parâmetros', () => {
     expect(() => {createItem()}).toThrow(Error);
   });
-  it('Lança um erro se o nome do item não é uma string', () => {
+  it('Teste: Lança um erro se o nome do item não é uma string', () => {
     expect(() => {createItem(1.99, 'kg', 'banana', 20);}).toThrow(new Error('O nome do item deve ser uma string'));
   });
-  it.todo('Lança um erro se o preço é negativo');
-  it.todo('Lança um erro se o preço é zero');
+  it('Teste: Lança um erro se o preço é negativo', () => {
+    expect(() => {createItem('banana', 'kg', -0.01, 20);}).toThrow(new Error('O preço do item deve ser maior que zero'));
+  });
+  it('Teste: Lança um erro se o preço é zero', () => {
+    expect(() => {createItem('banana', 'kg', 0, 20);}).toThrow(new Error('O preço do item deve ser maior que zero'));
+  });
 });

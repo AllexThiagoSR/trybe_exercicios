40146121592a919560355d173d774b315ac408b9
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-21 21:05:31 -0300

    Adiciona os teste de vogais e se existe a função encode

--- fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_2/exercicios.test.js
+++ fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_2/exercicios.test.js
@@ -1,4 +1,4 @@
-const {myRemove, myFizzBuzz} = require('./exercicios.js');
+const {myRemove, myFizzBuzz, encode, decode} = require('./exercicios.js');
 
 describe('Testes da função exercicio 1', () => {
   test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
@@ -36,4 +36,27 @@ describe('Testes da função do exercício 2', () => {
   it('Teste: Se for passado um valor não númerico retorna false', () =>{
     expect(myFizzBuzz('aa')).toBe(false);
   });
+});
+
+describe('Testes de encode função do exercício 3', () => {
+  const letters = ['a', 'e',  'i', 'o', 'u'];
+  const numbers = [1, 2, 3, 4 ,5];
+  const phrase = 'A trybe é da hora';;
+
+  it('Teste: Se existe a função encode', () => {
+    expect(typeof encode).toBe('function');
+  });
+
+  for (let index = 0; index < letters.length; index += 1) {
+    it(`Teste: Se a vogal "${letters[index]}" é convertida para ${numbers[index]}`, () => {
+      const regexLet = new RegExp(`/${letters[index]}/`, 'g');
+      const regexNum = new RegExp(`/${numbers[index]}/`, 'g');
+      const vowelMatches = phrase.match(regexLet);
+      if (vowelMatches !== null) {
+        expect(encode(phrase).match(regexNum).length).toBe(vowelMatches.length);
+      } else {
+        expect(0).toBe(0);
+      }
+    });
+  }
 });

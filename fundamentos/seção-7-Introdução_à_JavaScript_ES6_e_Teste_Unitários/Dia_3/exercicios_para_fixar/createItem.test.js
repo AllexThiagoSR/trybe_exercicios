1e38974c4db2c7838683fc9c277297e60fe1c477
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-22 16:04:10 -0300

    Testa se é lançado erro ao não enviar parâmetros

--- fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_3/exercicios_para_fixar/createItem.test.js
+++ fundamentos/seção-7-Introdução_à_JavaScript_ES6_e_Teste_Unitários/Dia_3/exercicios_para_fixar/createItem.test.js
@@ -6,8 +6,10 @@ describe('a função createItem', () => {
   it('Teste: Se ao não ser enviada uma quantidade utiliza zero como quantidade padrão', () => {
     expect(createItem('banana', 'kg', 1.99)).toHaveProperty('quantity', 0);
   });
-  it.todo('Lança um erro quando não recebe parâmetros');
-  it.todo('Lança um erro se o nome do item não é uma string');
+  it('Teste: Se lança um erro quando não recebe parâmetros', () => {
+    expect(() => {createItem()}).toThrow(Error);
+  });
+  // it('Lança um erro se o nome do item não é uma string', () => {});
   it.todo('Lança um erro se o preço é negativo');
   it.todo('Lança um erro se o preço é zero');
 });

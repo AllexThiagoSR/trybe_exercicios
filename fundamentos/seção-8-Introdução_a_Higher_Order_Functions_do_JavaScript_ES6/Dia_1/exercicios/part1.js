84bf168726761d26f13b206f060c8e973abbf41d
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-29 14:57:06 -0300

    Exercicio nova pessoa contratada resolvido

---
+++ fundamentos/seção-8-Introdução_a_Higher_Order_Functions_do_JavaScript_ES6/Dia_1/exercicios/part1.js
@@ -0,0 +1,12 @@
+const addEmployee = (name) => ({nome: name, email: name.replace(' ', '_').toLowerCase() + '@trybe.com'});
+
+const newEmployees = (func) => {
+  const employees = {
+    id1: func('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: func('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: func('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(addEmployee));

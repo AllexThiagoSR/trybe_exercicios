bb9805e024838cd1a005de90cd0f1cc3dce19b86
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-30 15:54:27 -0300

    Setup inicial do exercicio 2

---
+++ fundamentos/seção-8-Introdução_a_Higher_Order_Functions_do_JavaScript_ES6/Dia_2/exercicios/exercicio2.js
@@ -0,0 +1,9 @@
+const books = require('./list');
+const expectedResult = [
+  'George R. R. Martin - 1948',
+  'J. R. R. Tolkien - 1892',
+  'Isaac Asimov - 1920',
+  'Frank Herbert - 1920',
+  'Stephen King - 1947',
+  'H. P. Lovecraft - 1890',
+];

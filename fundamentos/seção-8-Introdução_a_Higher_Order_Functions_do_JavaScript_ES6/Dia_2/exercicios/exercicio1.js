034bc4693e623ff4484c6e8bd4b7b3cd67175362
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-30 15:33:27 -0300

    Setup do primeiro exercício

---
+++ fundamentos/seção-8-Introdução_a_Higher_Order_Functions_do_JavaScript_ES6/Dia_2/exercicios/exercicio1.js
@@ -0,0 +1,16 @@
+const books = require('./list');
+
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+const formatedBookNames = () => {
+  // escreva seu código aqui
+}
+
+console.log(books);

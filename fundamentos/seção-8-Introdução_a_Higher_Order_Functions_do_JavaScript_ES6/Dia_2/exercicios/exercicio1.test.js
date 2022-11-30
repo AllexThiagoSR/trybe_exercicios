d7382b6e5fb7b45274ed331f9d6fe1747de71042
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-30 15:48:12 -0300

    Implementa a função do primeiro exercício e o teste dela

---
+++ fundamentos/seção-8-Introdução_a_Higher_Order_Functions_do_JavaScript_ES6/Dia_2/exercicios/exercicio1.test.js
@@ -0,0 +1,17 @@
+const formatedBookNames = require('./exercicio1');
+const books = require('./list');
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+it('Testa o retorno da função formatedBookName', () => {
+  expect(formatedBookNames(books)).toEqual(expectedResult);
+  expect(formatedBookNames(books)).not.toEqual([]);
+  expect(formatedBookNames()).toEqual([]);
+  expect(formatedBookNames([])).toEqual([]);
+});

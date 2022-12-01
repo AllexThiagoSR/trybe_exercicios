8db68598b89dde7249fac36ccbcb9be57dd4ab70
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-01 18:40:00 -0300

    Exercicio 1 resolvido de 3 formas diferentes

---
+++ fundamentos/seção-8-Introdução_a_Higher_Order_Functions_do_JavaScript_ES6/Dia_3/exercise1.js
@@ -0,0 +1,59 @@
+const books = require('./books');
+
+const expectedResult = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965
+  }
+];
+
+const checkGenre = (first, second) => first === second; 
+
+// Utilizando o for
+// const fantasyOrScienceFiction = (books) => {
+//   const selectedBooks = [];
+
+//   for (const book of books) {
+//     const bookGenre = book.genre;
+//     if (bookGenre === 'Fantasia' || bookGenre === 'Ficção Científica') selectedBooks.push(book);
+//   }
+
+//   return selectedBooks;
+// };
+
+// Utilizadno HOF - filter
+
+const fantasyOrScienceFiction = (books) => books.
+filter((book) => (checkGenre(book.genre, 'Fantasia') || checkGenre(book.genre, 'Ficção Científica')));
+
+// Utilizando HOF - reduce
+// const fantasyOrScienceFiction = (books) => books.reduce((finalArray, book) => {
+//     if (checkGenre(book.genre, 'Fantasia') || checkGenre(book.genre, 'Ficção Científica')) finalArray.push(book);
+//     return finalArray
+//   }, []);
+
+console.log(fantasyOrScienceFiction(books));

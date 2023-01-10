2de593cf3b9c592e95a4519db3a9500df6e64c02
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-01-10 16:14:38 -0300

    Verifica o que foi digitado no input

--- 2-front-end/Dia_1/field-validator/src/script.js
+++ 2-front-end/Dia_1/field-validator/src/script.js
@@ -1,21 +1,27 @@
-import { 
+import {
   isEmail,
   isURL,
   isDate,
   isHexColor,
   isTaxID,
-} from 'validate';
+} from 'validator';
 
-const url = (str) => isURL(str);
-
-const email = (str) => isEmail(str);
-
-const data = (str) => isDate(str, { format: 'DD/MM/YYYY', delimeiters: ['/', '-'] });
-
-const hex = (str) => isHexColor(str);
-
-const cpf = (str) => isTaxID(str, 'pt-BR');
+const verifies = {
+  url: (str) => isURL(str),
+  email: (str) => isEmail(str),
+  data: (str) => isDate(str, { format: 'DD/MM/YYYY', delimeiters: ['/', '-'] }),
+  hex: (str) => isHexColor(str),
+  cpf: (str) => isTaxID(str, 'pt-BR'),
+};
 
 const input = document.querySelector('input');
 const select = document.querySelector('select');
 const verifyBtn = document.querySelector('button');
+const h2 = document.querySelector('h2');
+
+verifyBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const textToVerify = input.value;
+  const veriType = select.value;
+  h2.innerText = `A validação retornou ${verifies[veriType](textToVerify)}`;
+});

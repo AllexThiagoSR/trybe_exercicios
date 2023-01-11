3ed73124084258dfac92596a04968fb2f475e10e
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-01-11 15:22:43 -0300

    Finaliza a página

--- 2-front-end/Dia_2/microblog/src/script.js
+++ 2-front-end/Dia_2/microblog/src/script.js
@@ -10,28 +10,34 @@ import {
 
 const usersSelect = document.querySelector('#users-select');
 
-const addComments = (id) => {
-  fetch(`https://dummyjson.com/posts/${id}/comments`).then((response) => response.json()).then(({ comments }) => {
-    console.log(comments);
-    console.log(id);
-    fillFeaturedPostComments(comments);
-  });
+const addComments = (postId) => {
+  fetch(`https://dummyjson.com/posts/${postId}/comments`)
+    .then((response) => response.json())
+    .then(({ comments }) => {
+      fillFeaturedPostComments(comments);
+    })
+    .catch(({ message }) => fillErrorMessage(message));
 };
 
 const addPosts = (id) => {
-  fetch(`https://dummyjson.com/posts/user/${id}`).then((response) => response.json()).then(({ posts }) => {
-    console.log(posts);
-    fillPosts(posts);
-    addComments(posts[0].id);
-  });
+  fetch(`https://dummyjson.com/posts/user/${id}`)
+    .then((response) => response.json())
+    .then(({ posts }) => {
+      fillPosts(posts);
+      addComments(posts[0].id);
+    })
+    .catch(({ message }) => fillErrorMessage(message));
 };
 
 const USERS_API = 'https://dummyjson.com/users';
 // faça a lógica para pegar as informações das pessoas usuárias e preencher o select aqui.
 
-fetch(USERS_API).then((response) => response.json()).then(({ users }) => {
-  fillUsersSelect(users);
-});
+fetch(USERS_API)
+  .then((response) => response.json())
+  .then(({ users }) => {
+    fillUsersSelect(users);
+  })
+  .catch(({ message }) => fillErrorMessage(message));
 
 usersSelect.addEventListener('change', (event) => {
   clearPageData();

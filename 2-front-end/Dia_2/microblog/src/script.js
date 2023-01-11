f9e84a0606c07da7450e5e642d9970d7ce7a74f1
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-01-11 14:43:49 -0300

    Adiciona os posts da pessoa selecionada na página

--- 2-front-end/Dia_2/microblog/src/script.js
+++ 2-front-end/Dia_2/microblog/src/script.js
@@ -10,22 +10,20 @@ import {
 
 const usersSelect = document.querySelector('#users-select');
 
-const getPosts = (id) => {
-  let posts;
-  fetch(`https://dummyjson.com/posts/user/${id}`).then((response) => response.json()).then((data) => console.log(data));
-  return posts;
+const getPosts = (id, func) => {
+  fetch(`https://dummyjson.com/posts/user/${id}`).then((response) => response.json()).then(({ posts }) => {
+    func(posts);
+  });
 };
 
 const USERS_API = 'https://dummyjson.com/users';
 // faça a lógica para pegar as informações das pessoas usuárias e preencher o select aqui.
 
 fetch(USERS_API).then((response) => response.json()).then(({ users }) => {
-  console.log(users);
   fillUsersSelect(users);
 });
 
 usersSelect.addEventListener('change', (event) => {
   clearPageData();
-  getPosts(event.target.value);
-  // faça a lógica para pegar as informações dos posts da pessoa selecionada e dos comentários do post destacado aqui.
+  getPosts(event.target.value, fillPosts);
 });

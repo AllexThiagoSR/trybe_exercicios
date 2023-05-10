aab50952c0b92a982e7d9d26e2696d7841385ddb
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-05-10 20:21:13 -0300

    Aplicação agora diz em qual categoria o imc da pessoa se encaixa

--- 3-back-end/exercicios4.1/bmi.js
+++ 3-back-end/exercicios4.1/bmi.js
@@ -2,14 +2,56 @@ const readLine = require('readline-sync');
 
 const bmiCalculator = (weight, height) => {
   let heightInMeters = height;
-  if (height >= 100) {
-    heightInMeters /= 100;
-  }
-  return (weight / (heightInMeters ** 2)).toFixed(2);
+  if (height >= 100) heightInMeters /= 100;
+  const bmi = weight / (heightInMeters ** 2);
+  const categoryRanges = [
+    {
+      min: 0,
+      max: 18.4,
+      category: 'Abaixo do peso (magreza)'
+    },
+    {
+      min: 18.5,
+      max: 24.9,
+      category: 'Peso normal'
+    },
+    {
+      min: 25,
+      max: 29.9,
+      category: 'Acima do peso (sobrepeso)'
+    },
+    {
+      min: 30,
+      max: 34.9,
+      category: 'Obesidade grau I'
+    },
+    {
+      min: 35,
+      max: 39.9,
+      category: '	Obesidade grau II'
+    },
+    {
+      min: 40,
+      max: 0,
+      category: 'Obesidade graus III e IV'
+    },
+  ];
+
+  const category = categoryRanges.reduce((choosedCategory, { min, max, category }) => {
+    if (min === 0) return bmi >= min ? category : choosedCategory;
+    if (max === 0) return bmi <= max ? category : choosedCategory;
+    if (min <= bmi && bmi <= max) return category;
+    return choosedCategory;
+  }, '');
+
+  return {
+    bmi: bmi.toFixed(2),
+    category,
+  };
 };
 
 const weight = readLine.questionFloat('Digite seu peso em kg: ');
-const height = readLine.questionFloat('Digite sua altura em cm ou em m: ')
+const height = readLine.questionFloat('Digite sua altura em cm ou em m: ');
 
 console.log(bmiCalculator(weight, height));
 

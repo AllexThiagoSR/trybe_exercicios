f454681665e018acac686c1c194c6fcbf8c4f822
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-05-10 23:40:22 -0300

    Todas as questões da atividade 6

--- 3-back-end/exercicios4.1/managerSimpsons.js
+++ 3-back-end/exercicios4.1/managerSimpsons.js
@@ -1,13 +1,10 @@
-const { rejects } = require('assert');
-const { error } = require('console');
-
 const fs = require('fs').promises;
 
-const getSimpsons = async () => JSON.parse(await fs.readFile('./simpsons.json'));
+const getCharacters = async (path) => JSON.parse(await fs.readFile(path));
 
 const showAllSimpsons = async () => {
   try {
-    const data = await getSimpsons();
+    const data = await getCharacters('./simpsons.json');
     data.forEach(({ id, name }) => {
       console.log(`${id} - ${name}`);
     });
@@ -17,7 +14,7 @@ const showAllSimpsons = async () => {
 };
 
 const findSimpsonbyId = async (id) => {
-  const data = await getSimpsons();
+  const data = await getCharacters('./simpsons.json');
   return new Promise((resolve, reject) => {
     const founcCharacter = data.find((character) => id === parseInt(character.id));
     if (!founcCharacter) reject(new Error('id não encontrado'));
@@ -26,20 +23,39 @@ const findSimpsonbyId = async (id) => {
 };
 
 const removeCharacterById = async (...ids) => {
-  const data = await getSimpsons();
+  const data = await getCharacters('./simpsons.json');
   const filteredData = data.filter(({ id }) => !ids.includes(id));
   await fs.writeFile('./simpsons.json', JSON.stringify(filteredData));
 };
 
 const writeFileWithSimpsonFamily = async () => {
-  const data = await getSimpsons();
-  const onlyFamily = data.filter(({ id }) => 1 <= id <= 4);
+  const data = await getCharacters('./simpsons.json');
+  const onlyFamily = data.filter(({ id }) => 1 <= id && id <= 4);
   fs.writeFile('./simpsonFamily.json', JSON.stringify(onlyFamily));
 };
 
-showAllSimpsons();
-findSimpsonbyId(110)
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error.message));
-removeCharacterById(6, 10);
-writeFileWithSimpsonFamily();
+const addNewCharacterAtFamily = async (id) => {
+  const data = await getCharacters('./simpsons.json');
+  const family = await getCharacters('./simpsonFamily.json');
+  const newCharacter = data.find((character) => id === parseInt(character.id));
+  fs.writeFile('./simpsonFamily.json', JSON.stringify([...family, newCharacter]));
+};
+
+const replaceCharacter = async (firstId, secondId) => {
+  const data = await getCharacters('./simpsons.json');
+  const family = await getCharacters('./simpsonFamily.json');
+  const newCharacter = data.find((character) => secondId === parseInt(character.id));
+  const newFamily = family
+    .map((character) => parseInt(character.id) === firstId ? newCharacter : character);
+  console.log(newFamily);
+  fs.writeFile('./simpsonFamily.json', JSON.stringify(newFamily));
+}
+
+// showAllSimpsons();
+// findSimpsonbyId(110)
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error.message));
+// removeCharacterById(6, 10);
+// writeFileWithSimpsonFamily();
+// addNewCharacterAtFamily(8);
+replaceCharacter(8, 5);
